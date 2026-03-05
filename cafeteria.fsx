1f138b4f1a1645f8e06264b5881d79f71242ae8d
BusinessBiggie <snappysnakes@gmail.com>
2026-03-05 20:55:34 +0000

    Sprint 2, part 1 and 2

--- cafeteria.fsx
+++ cafeteria.fsx
@@ -27,45 +27,45 @@ let getDrinkBasePrice drink =
     match drink with
     | Coffee (variety, _) -> 
         match variety with
-        | Espresso   -> 15.0m
-        | Latte      -> 25.0m
-        | Cappuccino -> 25.0m 
-        | Americano  -> 20.0m 
-        | Filter     -> 12.0m
+        | Espresso   -> 15
+        | Latte      -> 25
+        | Cappuccino -> 25 
+        | Americano  -> 20 
+        | Filter     -> 12
 
     | Tea (variety, _) ->
         match variety with
-        | Green    -> 15.0m
-        | Black    -> 15.0m
-        | Herbal   -> 14.0m
-        | Oolong   -> 18.0m
-        | Rooibus  -> 13.0m
+        | Green    -> 15
+        | Black    -> 15
+        | Herbal   -> 14
+        | Oolong   -> 18
+        | Rooibus  -> 13
 
     | Juice variety ->
         match variety with
-        | Orange     -> 15.0m
-        | Apple      -> 15.0m
-        | Multifruit -> 18.0m
-        | Exotic     -> 18.0m
+        | Orange     -> 15
+        | Apple      -> 15
+        | Multifruit -> 18
+        | Exotic     -> 18
 
     | Milk variety ->
         match variety with
-        | Skummet -> 10.0m
-        | Mini    -> 10.0m
-        | Let     -> 10.0m
+        | Skummet -> 10
+        | Mini    -> 10
+        | Let     -> 10
 
     | Soda variety ->
         match variety with
-        | Fanta     -> 15.0m
-        | Pepsi     -> 20.0m
-        | Sprite    -> 18.0m
-        | FaxeKondi -> 15.0m
+        | Fanta     -> 15
+        | Pepsi     -> 20
+        | Sprite    -> 18
+        | FaxeKondi -> 15
 
 let applyDrinkSize size price =
     match size with
     | Small  -> price
-    | Medium -> price + 5.0m
-    | Large  -> price + 10.0m
+    | Medium -> price + 5
+    | Large  -> price + 10
 
 let calculateDrinkPrice (drink: Drink) =
     match drink with
@@ -82,13 +82,13 @@ let calculateDrinkPrice (drink: Drink) =
 
 let calculateFoodPrice food =
     match food with
-    | Sandwich -> 45.0m
-    | Pastry   -> 20.0m
-    | Salad    -> 35.0m
+    | Sandwich -> 45
+    | Pastry   -> 20
+    | Salad    -> 35
 
 let calculateFruitPrice fruit =
     match fruit with
-    | Grapes | Banana | Pear -> 5.0m
+    | Grapes | Banana | Pear -> 5
 
 
 
@@ -98,5 +98,35 @@ let myMilkOrder   = Milk(Skummet)
 let coffeePrice = myCoffeeOrder |> calculateDrinkPrice
 let milkPrice   = myMilkOrder   |> calculateDrinkPrice
 
-printfn "Coffee: %A - Price: %M DKK" myCoffeeOrder coffeePrice
-printfn "Milk: %A - Price: %M DKK" myMilkOrder milkPrice
+printfn "Coffee: %A - Price: %int DKK" myCoffeeOrder coffeePrice
+printfn "Milk: %A - Price: %int DKK" myMilkOrder milkPrice
+
+//SPRINT 2
+type PaymentType = Cash | Card | MobilePay | ApplePay | GooglePay
+
+type Customer = {
+    Id: int
+    Name: string
+    Email: string
+    Phone: string
+}
+
+let gtgVAT (x: int) =
+    let percentage = 0.25
+    float x + (float x * percentage)
+
+printfn "Price with VAT: %.2f DKK" (gtgVAT coffeePrice)
+
+type OrderProductMsg = 
+    | OrderDrink of Drink * qty:int 
+    | OrderFood of Food * qty:int
+    | OrderFruit of Fruit * qty:int
+    | LeaveComment of string
+
+let gtgAgent = MailboxProcessor<OrderProductMsg>.Start(fun inbox ->
+    let rec loop () = async {
+        //Part 3
+        return ()
+    }
+    loop ()
+)
